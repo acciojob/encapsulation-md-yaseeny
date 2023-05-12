204da767844a51341422ca3ed3fc6e8c80ef06d9
Md Yaseen <mdyaseeny589@gmail.com>
2023-05-13 00:54:36 +0530

    encapsulation assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+//        rwOnly.name = "Yaseen";
+        // error : java: name has private access in com.driver.RWOnly
+        rwOnly.setName("Yaseen");
+        String name = rwOnly.getName();
+    }
 }
